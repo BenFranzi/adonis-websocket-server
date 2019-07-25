abf5aaccc26a6c91b6cbc2430527ab01ad03da08
Ben Franzi <benfranzi@outlook.com>
2019-07-25 16:42:39 +1000

    changed were made

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -1,10 +1,13 @@
 'use strict'
 
 class ChatController {
+  userId;
+
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
-    console.log('user joined with %s socket id', socket.id)
+    console.log('user joined with %s socket id', socket.id);
+    setTimeout(this.emitCommand.bind(this), 10000);
   }
 
   onMessage (message) {
@@ -16,8 +19,18 @@ class ChatController {
     this.socket.broadcastToAll('port', this.socket.id.charCodeAt(this.socket.id.length - 1));
   }
 
-  onPing() {
-    console.log('on ping')
+  emitCommand() {
+    console.log('sending command');
+    this.socket.broadcastToAll('command', {
+      //msg_type: 'command', // Not required as the message type is the topic emitted
+      msg_type_version: 1,
+      command_id: '123ABC456DEF',
+      display_text: 'Hello this is Patrick',
+      gcode: 'ECHO "Hello, world!"',
+      priority: 5, //X, 0, 1, 2, 3, 4, 5, 6, etc (Higher number is lower priority)
+      timeout_s: 30, //0 is no timeout, time in seconds
+      cmd_type: 'UserAcceptCancel',
+    });
   }
 }
 
