ab1ed83321b817ec947909a6dbf0931e116103f8
Jimmy Tang <blay.tenshi@gmail.com>
2019-08-05 15:19:49 +1000

    Printer controller now has a response listener

--- app/Controllers/Ws/PrinterController.js
+++ app/Controllers/Ws/PrinterController.js
@@ -16,16 +16,20 @@ class PrinterController {
   onPort (message) {
     console.log('\nsocket:', this.socket.id, '\nport:  ', this.socket.id.charCodeAt(this.socket.id.length - 1),'\n');
     this.socket.broadcastToAll('port', this.socket.id.charCodeAt(this.socket.id.length - 1));
+    //this.emitCommand(this);
   }
 
-  onAcceptUser (message) {
-    console.log('User accept message received');
+  onAcceptuser (message) {
+    console.log('User accept message received', message);
   }
-  
-  onRejectUser (message) {
+
+  onRejectuser (message) {
     console.log('User reject message received');
   }
 
+  onResponse (response) {
+    console.log('Response', response);
+  }
 
   emitCommand() {
     console.log('sending command');
@@ -33,10 +37,10 @@ class PrinterController {
       //msg_type: 'command', // Not required as the message type is the topic emitted
       msg_type_version: 1,
       command_id: '123ABC456DEF',
-      display_text: 'Hello this is Patrick',
+      display_text: 'Request to print file: File.gcode',
       gcode: 'ECHO "Hello, world!"',
       priority: 5, //X, 0, 1, 2, 3, 4, 5, 6, etc (Higher number is lower priority)
-      timeout_s: 30, //0 is no timeout, time in seconds
+      timeout_s: 10, //0 is no timeout, time in seconds
       cmd_type: 'UserAcceptCancel',
     });
     setTimeout(this.emitCommand.bind(this), 30000);
